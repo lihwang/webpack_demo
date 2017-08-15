98016d45e82bd3a6b5948b32e724a1e3292264b7
lichengjie <lichengjie@wdai.com>
2017-08-15 14:37:09 +0800

    配置文件和webpack文件

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { hello, hello1 } from './script/hello1.js';
+import { demo } from './script/hello.js';
+import './style/cssdemo.css';
+import './style/lessdemo.less';
+import './style/sassdemo.scss';
+import './style/stylusdemo.styl';
