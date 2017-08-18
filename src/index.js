11dc72e901d733048c000d5215289af45e4efa31
lichengjie <lichengjie@wdai.com>
2017-08-18 10:21:09 +0800

    跟新代码

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import { hello, hello1 } from './script/hello1.js';
-import { demo } from './script/hello.js';
+import { hello, hello1 } from './script/hello1.jsx';
+import { demo } from './script/hello.jsx';
 import './style/cssdemo.css';
 import './style/lessdemo.less';
 import './style/sassdemo.scss';
 import './style/stylusdemo.styl';
+
+import React from 'react'
+import { render } from 'react-dom'
+import App from './script/App.jsx'
---
+++ src/script/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//     }
+//     render() {
+//         return (<div>Hello, React Router!</div>)
+//     }
+// }
+let App = 1;
+export default App;
